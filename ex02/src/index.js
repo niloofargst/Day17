3f03d591bb215cc06fa931e3295ec9de375a5360
Niloo <niloofargst@gmail.com>
2021-03-25 09:30:22 +0100

    adding ex01-06

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(myCar) {
+    // Only change code below this line
+
+    const { cylinders } = MY_CAR;
+    const { size } = MY_CAR;
+
+    return { cylinders, size };
+
+    // Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
